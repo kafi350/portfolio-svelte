f3b448c45513258cd7f2e85f6af980bd408b230b
kafi350 <kafiabdullahil96@gmail.com>
2024-10-22 21:23:19 +0200

    icon and navbar change

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,46 +1,54 @@
 <script>
     import {
-    Collapse,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    Navbar,
-    NavItem,
-    NavLink,
-  } from '@sveltestrap/sveltestrap';
-
-  export let navlist;
-  export let personal;
-
-  let isOpen = false;
-
-  function handleUpdate(event) {
-    isOpen = event.detail.isOpen;
-  }
-</script>
-
-<header id="header">
-    <Navbar id="navbar" color="light" light expand="md" container="md">
-        <NavbarBrand href="/">{personal.name}</NavbarBrand>
-        <NavbarToggler on:click={() => (isOpen = !isOpen)} />
-        <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
-            <Nav class="ms-auto" navbar>
-            {#each navlist as item}
-                <NavItem>
-                    <NavLink href={item.url}>{item.label}</NavLink>
-                </NavItem>
-            {/each}
-            </Nav>
-        </Collapse>
-    </Navbar>
-</header>
-
-<style>
-    #header {
-        padding-top: 64px;
-        padding-bottom: 64px;
-        margin: 0;
-        font-family: JetBrainsMono-Bold;
+      Collapse,
+      NavbarToggler,
+      NavbarBrand,
+      Nav,
+      Navbar,
+      NavItem,
+      NavLink,
+    } from '@sveltestrap/sveltestrap';
+  
+    export let navlist;
+    export let personal;
+  
+    let isOpen = false;
+  
+    function toggleNavbar() {
+      isOpen = !isOpen;
     }
-
-</style>
+  
+    function closeNavbar() {
+      isOpen = false;  // Close the menu when a nav item is clicked
+    }
+  </script>
+  
+  <header id="header">
+    <Navbar id="navbar" color="light" light expand="md" container="md">
+      <NavbarBrand href="/">{personal.name}</NavbarBrand>
+      <NavbarToggler on:click={toggleNavbar} />
+      <Collapse {isOpen} navbar expand="md">
+        <Nav class="ms-auto" navbar>
+          {#each navlist as item}
+            <NavItem>
+              <NavLink href={item.url} on:click={closeNavbar}>
+                {item.label}
+              </NavLink>
+            </NavItem>
+          {/each}
+        </Nav>
+      </Collapse>
+    </Navbar>
+  </header>
+  
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@700&display=swap');
+    
+    #header {
+      padding-top: 64px;
+      padding-bottom: 64px;
+      margin: 0;
+      font-family: 'JetBrains Mono', monospace;
+    }
+  </style>
+  
